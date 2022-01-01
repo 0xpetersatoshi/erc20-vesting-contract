3e615f97dba82d956c43a1655e6ecf9fca3379b1
0xPeterSatoshi <0xpetersatoshi@protonmail.com>
2022-01-01 11:32:19 -0800

    update hardhat config with mnemonic phrase

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    hardhat: {
+      accounts: {
+        mnemonic:
+          process.env.MNEMONIC !== undefined ? process.env.MNEMONIC : "",
+      },
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
